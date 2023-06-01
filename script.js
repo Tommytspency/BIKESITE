8c056f06d356ff89bec85b093edd5d27ce11537a
Tom <tomspencerprimary@gmail.com>
2023-06-02 04:06:48 +1200

    pass

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ function displaySong1() {
   p.style.display = "block";  	 
 }
 
+function validate_password() {
+ 
+  var pass = document.getElementById('pass').value;
+  var confirm_pass = document.getElementById('confirm_pass').value;
+  if (pass != confirm_pass) {
+      document.getElementById('wrong_pass_alert').style.color = 'red';
+      document.getElementById('wrong_pass_alert').innerHTML
+          = 'Passwords do not match';
+      
+  } else {
+      document.getElementById('wrong_pass_alert').style.color = 'green';
+      document.getElementById('wrong_pass_alert').innerHTML =
+          'Password Matched';
+      
+  }
+}
+
