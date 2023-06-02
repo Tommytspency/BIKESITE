7bcc588c6502fbf36c81e669eae9d263fe5b852c
Tom <tomspencerprimary@gmail.com>
2023-06-02 22:14:08 +1200

    donenot

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-function displaySong1() {
-  var p = document.getElementById("congratulations");
-  p.style.display = "block";  	 
+function validateForm() {
+  
+    var username = document.forms["register"]["username"].value;
+    var password = document.forms["register"]["password"].value;
+    if (username == null || username == "") {
+        alert("Please enter all credentials");
+        return false;
+    }
+
+   else if (password == null || password == "") {
+    alert("Please enter all credentials");
+   return false;
 }
+
+    else
+    var p = document.getElementById("congratulations");
+    p.style.display = "block"; 
+    return false;
+}
+
 
 function displaySong2() {
   var p = document.getElementById("hired");
@@ -16,8 +32,9 @@ function checkPassword(){
 
   if(password.length != 0){
       if(password == cnfrmPassword){
-          message.textContent = "Wow, Passwords match";
+          message.textContent = "You are all signed up!";
           message.style.backgroundColor = "#1dcd59";
+          
       }
       else{
           message.textContent = "Sorry, Password don't match";
@@ -30,3 +47,30 @@ function checkPassword(){
   }
 }
 
+
+//this outlines that the bike has been rented and is ready to pick up from location.
+// electric scooters will have the same model as companies like Flamingo and Beam, where the scooters will be scattered 
+// around town for people to rent on location. So cannot be rented through the website, and will be paired with an app.
+
+
+function checkbiketype(){
+  let bikes = document.getElementById("bikes").value;
+  let elecscoot = document.getElementById("elecscoot").value;
+  let message = document.getElementById("message2");
+  console.log("Choose a bike:", bikes,'\n',"Electric Scooter", elecscoot);
+
+  {
+      if(bikes == elecscoot){
+          message.textContent = "Electric scooter can only be rented from app!";
+          message.style.backgroundColor = "#ff4d4d";
+          
+      }
+
+      else{
+        message.textContent = "Rented!";
+        message.style.backgroundColor = "#1dcd59";
+    }
+
+     
+    }
+  }
